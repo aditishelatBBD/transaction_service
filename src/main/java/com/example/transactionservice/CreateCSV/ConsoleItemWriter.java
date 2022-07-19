e2ce0d9c817cb0dead799069c91c4c14e32ff931
aditishelatBBD <aditi@bbd.co.za>
2022-07-19 16:16:40 +0530

    readed multiple csv data

--- src/main/java/com/example/transactionservice/CreateCSV/ConsoleItemWriter.java
+++ src/main/java/com/example/transactionservice/CreateCSV/ConsoleItemWriter.java
@@ -1,4 +1,5 @@
 package com.example.transactionservice.CreateCSV;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.batch.item.ItemWriter;
@@ -6,8 +7,14 @@ import org.springframework.batch.item.ItemWriter;
 public class ConsoleItemWriter<T> implements ItemWriter<T>{
     @Override
     public void write(List<? extends T> items) throws Exception {
+        String[] array = new String[0];
         for (T item : items) {
-            System.out.println(item);
+            System.out.println("----------"+item);
+            array = items.toArray(new String[0]);
+//            System.out.println(array);
+        }
+        for (String eachString : array) {
+            System.out.println(eachString);
         }
     }
 }
